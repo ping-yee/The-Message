f82f5c15aa7d63ce4023e8b30d75c2a9891f5ea9
ping-yee <611077101@mail.nknu.edu.tw>
2023-10-11 18:54:49 +0800

    fix: resolve the confluct

--- Backend/service/repository/playerRepository.go
+++ Backend/service/repository/playerRepository.go
@@ -1,5 +1,6 @@
 package repository
 
+<<<<<<< HEAD
 import (
 	"context"
 )
@@ -13,4 +14,20 @@ type Player struct {
 
 type PlayerRepository interface {
 	CreatePlayer(ctx context.Context, player *Player) (*Player, error)
+=======
+import "context"
+
+type Player struct {
+	Id   int `gorm:"primaryKey;auto_increment"`
+	Name string
+	G_Id int
+}
+
+type PlayerRepository interface {
+	GetPlayerById(ctx context.Context, id int) (*Player, error)
+	PlayerDrawCards(ctx context.Context, id int, count int) ([]string, error)
+	UpdatePlayerCards(ctx context.Context, id int, cards []string) error
+	PlayerPlayCard(ctx context.Context, id int, card string) error
+	DeleteAllGamePlayers(ctx context.Context, id int) error
+>>>>>>> e21ca85 (feat: initial the player componenet.)
 }
